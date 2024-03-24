0b31b7c7294f4ee484a305f0a07a08e7298376e1
kovalev Andrey <andremae10@gmail.com>
2024-03-24 13:37:21 +0300

    first working version

---
+++ AccelerometrHash/src/test/java/kal/fit/bstu/accelerometrhash/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package kal.fit.bstu.accelerometrhash
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
