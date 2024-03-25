2e584ed975562345aae79ce6121a26224e7f7713
kovalev Andrey <andremae10@gmail.com>
2024-03-25 07:41:25 +0300

    second test to work with jitpack

--- AccelerometrHash/build.gradle.kts
+++ AccelerometrHash/build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from (components["release"])
+                version = System.getenv("GIT_TAG") ?: "1.0.0"
+            }
+        }
+    }
+}
 android {
+
     namespace = "kal.fit.bstu.accelerometrhash"
     compileSdk = 34
 
@@ -30,14 +42,4 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
