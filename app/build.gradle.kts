bef3c09bee6dfa4719a9baf95df0f46e05d81df1
kovalev Andrey <andremae10@gmail.com>
2024-03-25 15:43:33 +0300

    add plugin maven-publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,29 @@
 plugins {
+    id("maven-publish")
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.lolipodass"
+                artifactId = "accelerometrhash"
+                version = "1.0.0"
+            }
+        }
+    }
+}
+
 android {
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
     namespace = "kaler.accelerometrhash"
     compileSdk = 34
 
@@ -30,9 +50,4 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
